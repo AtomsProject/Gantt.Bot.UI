b2f70019b775cfea87ec7070ab3c47f861041dee
Adam Larsen <AdamCLarsen@users.noreply.github.com>
2024-03-09 21:20:34 -0600

    Don't show Tan Stack Router in production

--- src/src/routes/__root.tsx
+++ src/src/routes/__root.tsx
@@ -1,20 +1,20 @@
-﻿import {createRootRoute, Link, Outlet} from '@tanstack/react-router'
-import {TanStackRouterDevtools} from '@tanstack/router-devtools'
+﻿import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import AppNavBar from "../AppNavBar.tsx";
 import '../App.css';
-import {ProjectProvider} from "../components/ProjectSelectionContext.tsx";
-import {TaskProvider} from "../components/TaskContext.tsx";
+import { ProjectProvider } from "../components/ProjectSelectionContext.tsx";
+import { TaskProvider } from "../components/TaskContext.tsx";
 
 export const Route = createRootRoute({
     component: () => (
         <>
             <ProjectProvider>
                 <TaskProvider>
-                    <AppNavBar/>
-                    <Outlet/>
+                    <AppNavBar />
+                    <Outlet />
                 </TaskProvider>
             </ProjectProvider>
-            <TanStackRouterDevtools/>
+            {process.env.NODE_ENV !== 'production' && <TanStackRouterDevtools />}
         </>
     ),
 })
